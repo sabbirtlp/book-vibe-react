ddb4b8f6e68302b63aef193ce3b5e6026307e235
sabbirtlp <sabbirtlp@gmail.com>
2024-12-29 02:06:31 +0600

    Error page Changed

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -3,7 +3,11 @@
 const ErrorPage = () => {
     return (
         <div>
-            <h1>404 Error</h1>
+            <section className="h-[100vh] bg-[#F9F9FF]">
+                <div className="container mx-auto text-center">
+                    <h1 className="md:text-5xl text-4xl font-bold text-black">404</h1>
+                </div>
+            </section>
         </div>
     );
 };
